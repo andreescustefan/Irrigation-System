d649b9b1be5a4f942bb4d85b1fc4c0a441fe224f
andreescustefan <52623574+andreescustefan@users.noreply.github.com>
2021-03-18 16:36:03 +0200

    Add files via upload

--- irrigationsystem.py
+++ irrigationsystem.py
@@ -10,28 +10,30 @@ from gpiozero import LED
 API_write = 'TABU7D2OUI5TVGG5'
 baseURL = 'https://api.thingspeak.com/update?api_key=%s' % API_write
 
-out = -1
+#wet = -1
 
 #GPIO_SETUP
-soil = 17
 GPIO.setmode(GPIO.BCM)
+
+soil = 17
 GPIO.setup(soil, GPIO.IN)
 
 relay = 15
 GPIO.setup(relay, GPIO.OUT)
+GPIO.output(relay, 1)
 
 #Soil_Moisture
-def measure():
+def get_status():
+    global soil
     if GPIO.input(soil):
         print("no water detected")
-        GPIO.output(relay, 1)
+        pump(1)
         return 0
     else:
         print("water detected")
-        GPIO.output(relay, 0)
-        GPIO.cleanup()
+        pump(0)
         return 1
-        
+
 #DHT22
 def DHT22_data():
     humi, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 16)
@@ -39,18 +41,26 @@ def DHT22_data():
     print(temp)
     return humi, temp
 
+def pump(turn):
+    global relay
+    if turn == 1:
+        GPIO.output(relay, 1)
+    else:
+        GPIO.output(relay, 0)
+
 while True:
     try:
         humi, temp = DHT22_data()
-        out = measure()
+        wet = get_status()
         if isinstance(humi, float) and isinstance(temp, float):
             humi = '%.2f' % humi
             temp = '%.2f' % temp
-            conn = urllib2.urlopen(baseURL + '&field1=%s&field2=%s&field3=%s' % (humi, temp, out))
+            conn = urllib2.urlopen(baseURL + '&field1=%s&field2=%s&field3=%s' % (humi, temp, wet))
             conn.close()
         else:
             print ('Error')
             sleep(100)
     except:
         break
+
 GPIO.cleanup()
