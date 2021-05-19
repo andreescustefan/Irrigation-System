b07687622cf8074e4389248d9665e3f47a55a6f7
andreescustefan <52623574+andreescustefan@users.noreply.github.com>
2021-05-19 16:32:51 +0300

    UI pentru setarea programului
    Am rearanjat codul si am adaugat partea de UI pentru setarea unui program de lucru al sistemului de irigare.

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,42 +1,117 @@
 package com.example.kotliniot
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.okhttp.OkHttpClient
 import com.squareup.okhttp.Request
 import com.squareup.okhttp.Response
+import kotlinx.android.synthetic.main.activity_main.*
 import java.io.IOException
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     private val handler = Handler()
-    private fun getAllData(x: TextView, y: TextView, z: TextView, a: TextView, b: TextView) {
-         val runnable: Runnable = object : Runnable {
-            override fun run() {
-                getData(x, "2", 2)
-                getData(y, "1", 1)
-                getData(z, "3", 3)
-                getData(a, "4", 4)
-                getData(b, "5", 5)
 
-                val textViewInfoAuto = findViewById<TextView>(R.id.textViewInfoAuto)
-                val textViewInfoPump = findViewById<TextView>(R.id.textViewInfoPump)
-                val toggleAuto = findViewById<ToggleButton>(R.id.toggleButton)
-                val togglePump = findViewById<ToggleButton>(R.id.toggleButtonPump)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        onClickTime()
+        onClickDuration()
+
+        toggleAuto.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                sendData("4=1")
+                toggleManual.isChecked = false
+                toggleSchedule.isChecked = false
+            }
+        }
+        toggleManual.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                sendData("4=0")
+                toggleAuto.isChecked = false
+                toggleSchedule.isChecked = false
+                turnManualOptions(1)
+            }
+            else{
+                turnManualOptions(0)
+            }
+        }
+        toggleSchedule.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                sendData("4=2")
+
+                toggleAuto.isChecked = false
+                toggleManual.isChecked = false
+
+                turnScheduleOptions(1)
+            }
+            else {
+                turnScheduleOptions(0)
+            }
+        }
+        togglePump.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                sendData("5=1")
+            } else {
+                sendData("5=0")
+            }
+        }
+        getAllData()
+
+    }
+    private fun getAllData() {
+        val runnable: Runnable = object : Runnable {
+            @SuppressLint("SetTextI18n")
+            override fun run() {
+                getData(textViewTemp, "2", 2)
+                getData(textViewHum, "1", 1)
+                getData(textViewSoil, "3", 3)
+                getData(textViewInfoAuto, "4", 4)
+                getData(textViewInfoPump, "5", 5)
+
                 val valueAuto = textViewInfoAuto.text.toString().toIntOrNull()
                 val valuePump = textViewInfoPump.text.toString().toIntOrNull()
 
                 if(valueAuto == 0){
+                    toggleManual.isChecked = true
                     toggleAuto.isChecked = false
-                    togglePump.isEnabled = true
+                    toggleSchedule.isChecked = false
+
+                    turnManualOptions(1)
+                    turnScheduleOptions(0)
+
+                    textViewMode.text = "Manual mode"
+                    textViewMode.setTextColor(Color.RED)
                 }
                 else if(valueAuto == 1){
+                    toggleManual.isChecked = false
                     toggleAuto.isChecked = true
-                    togglePump.isEnabled = false
+                    toggleSchedule.isChecked = false
+
+                    turnManualOptions(0)
+                    turnScheduleOptions(0)
+
+                    textViewMode.text = "Auto mode"
+                    textViewMode.setTextColor(Color.RED)
+                }
+                else if(valueAuto == 2){
+                    toggleManual.isChecked = false
+                    toggleAuto.isChecked = false
+                    toggleSchedule.isChecked = true
+
+                    turnManualOptions(0)
+                    turnScheduleOptions(1)
+
+                    textViewMode.text = "Schedule mode"
+                    textViewMode.setTextColor(Color.RED)
                 }
                 if(valuePump == 0){
                     togglePump.isChecked = false
@@ -49,14 +124,15 @@ class MainActivity : AppCompatActivity() {
             }
         }
         handler.post(runnable)
+
     }
-    private fun sendDada(field: String) {
+    private fun sendData(field: String) {
         val asyncTask: AsyncTask<*, *, *> = object : AsyncTask<Any?, Any?, Any?>() {
             override fun doInBackground(objects: Array<Any?>): Any? {
                 val client = OkHttpClient()
                 val request: Request = Request.Builder()
-                        .url("https://api.thingspeak.com/update?api_key=TABU7D2OUI5TVGG5&field" + field)
-                        .build()
+                    .url("https://api.thingspeak.com/update?api_key=TABU7D2OUI5TVGG5&field" + field)
+                    .build()
                 var response: Response? = null
                 try {
                     response = client.newCall(request).execute()
@@ -73,8 +149,8 @@ class MainActivity : AppCompatActivity() {
             override fun doInBackground(objects: Array<Any?>): Any? {
                 val client = OkHttpClient()
                 val request: Request = Request.Builder()
-                        .url("https://api.thingspeak.com/channels/939407/fields/" + field + "/last.json?api_key=GATCL5J9R1VLH7ML&results=2")
-                        .build()
+                    .url("https://api.thingspeak.com/channels/939407/fields/" + field + "/last.json?api_key=GATCL5J9R1VLH7ML&results=2")
+                    .build()
                 var response: Response? = null
                 try {
                     response = client.newCall(request).execute()
@@ -87,16 +163,14 @@ class MainActivity : AppCompatActivity() {
 
             override fun onPostExecute(o: Any?) {
                 val data = o.toString()
-                var hum: String
                 if(z == 2) {
-                    txt.text = (o.toString().substring(data.lastIndexOf("\":\"")+3, data.lastIndexOf("\""))) + " \u2103"
+                    txt.text = o.toString().substring(data.lastIndexOf("\":\"") + 3, data.lastIndexOf("\"")) + " \u2103"
                 }
                 if(z == 1) {
-                    hum = o.toString().substring(data.lastIndexOf("\":\"")+3, data.lastIndexOf("\"")) + "%"
-                    txt.text = hum
+                    txt.text = o.toString().substring(data.lastIndexOf("\":\"") + 3, data.lastIndexOf("\"")) + "%"
                 }
                 if(z ==3){
-                    val soil = o.toString().substring(data.lastIndexOf("\":\"")+3, data.lastIndexOf("\"")).toInt()
+                    val soil = o.toString().substring(data.lastIndexOf("\":\"") + 3, data.lastIndexOf("\"")).toInt()
                     if(soil==0){
                         txt.text = "Dry"
                     }
@@ -105,44 +179,88 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 if(z == 4) {
-                    txt.text = (o.toString().substring(data.lastIndexOf("\":\"")+3, data.lastIndexOf("\"")))
+                    txt.text = o.toString().substring(data.lastIndexOf("\":\"") + 3, data.lastIndexOf("\""))
                 }
                 if(z == 5) {
-                    txt.text = (o.toString().substring(data.lastIndexOf("\":\"")+3, data.lastIndexOf("\"")))
+                    txt.text = o.toString().substring(data.lastIndexOf("\":\"") + 3, data.lastIndexOf("\""))
                 }
             }
         }.execute()
+
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private fun onClickTime() {
+        timePicker.setIs24HourView(true)
+        timePicker.setOnTimeChangedListener { _, hour, minute -> var hour = hour
+            if (textViewTypeTime != null) {
+                val hour = hour
+                val min = if (minute < 10) "0$minute" else minute
 
-        val toggleAuto = findViewById<ToggleButton>(R.id.toggleButton)
-        val togglePump = findViewById<ToggleButton>(R.id.toggleButtonPump)
-        val textViewT = findViewById<TextView>(R.id.textViewTemp)
-        val textViewH = findViewById<TextView>(R.id.textViewHum)
-        val textViewDW = findViewById<TextView>(R.id.textViewSoil)
-        val textViewInfoAuto = findViewById<TextView>(R.id.textViewInfoAuto)
-        val textViewInfoPump = findViewById<TextView>(R.id.textViewInfoPump)
-
-        toggleAuto.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                sendDada("4=1")
-                togglePump.isEnabled = false
-            } else {
-                sendDada("4=0")
-                togglePump.isEnabled = true
-            }
-        }
-        togglePump.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                sendDada("5=1")
-            } else {
-                sendDada("5=0")
+                val msg = "$hour : $min "
+                textViewTypeTime.text = msg
+                textViewTypeTime.visibility = ViewGroup.VISIBLE
             }
         }
 
-        getAllData(textViewT, textViewH, textViewDW, textViewInfoAuto, textViewInfoPump)
+    }
+    private fun onClickDuration() {
+        durationPicker.setIs24HourView(true)
+        durationPicker.setOnTimeChangedListener { _, hour, minute -> var hour = hour
+            if (textViewTypeDuration != null) {
+                val hour = hour
+                val min = if (minute < 10) "0$minute" else minute
+
+                val msg = "$hour h and $min min"
+                textViewTypeDuration.text = msg
+                textViewTypeDuration.visibility = ViewGroup.VISIBLE
+            }
+        }
 
     }
+    private fun turnScheduleOptions(turn: Int){
+        if(turn == 0){
+            textViewStartTime.visibility = View.INVISIBLE
+            textViewDuration.visibility = View.INVISIBLE
+            textViewRepeat.visibility = View.INVISIBLE
+            textViewTypeDuration.visibility = View.INVISIBLE
+            durationPicker.visibility = View.INVISIBLE
+            textViewTypeTime.visibility = View.INVISIBLE
+            timePicker.visibility = View.INVISIBLE
+            checkBoxMo.visibility = View.INVISIBLE
+            checkBoxTu.visibility = View.INVISIBLE
+            checkBoxWe.visibility = View.INVISIBLE
+            checkBoxTh.visibility = View.INVISIBLE
+            checkBoxFr.visibility = View.INVISIBLE
+            checkBoxSa.visibility = View.INVISIBLE
+            checkBoxSu.visibility = View.INVISIBLE
+        }
+        else{
+            textViewStartTime.visibility = View.VISIBLE
+            textViewDuration.visibility = View.VISIBLE
+            textViewRepeat.visibility = View.VISIBLE
+            textViewTypeDuration.visibility = View.VISIBLE
+            durationPicker.visibility = View.VISIBLE
+            textViewTypeTime.visibility = View.VISIBLE
+            timePicker.visibility = View.VISIBLE
+            checkBoxMo.visibility = View.VISIBLE
+            checkBoxTu.visibility = View.VISIBLE
+            checkBoxWe.visibility = View.VISIBLE
+            checkBoxTh.visibility = View.VISIBLE
+            checkBoxFr.visibility = View.VISIBLE
+            checkBoxSa.visibility = View.VISIBLE
+            checkBoxSu.visibility = View.VISIBLE
+        }
+
+    }
+    private fun turnManualOptions(turn: Int){
+        if(turn == 0){
+            textViewPump.visibility = View.INVISIBLE
+            togglePump.visibility = View.INVISIBLE
+        }
+        else{
+            textViewPump.visibility = View.VISIBLE
+            togglePump.visibility = View.VISIBLE
+        }
+
+    }
+
 }
